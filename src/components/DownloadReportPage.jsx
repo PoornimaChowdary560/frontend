86b742e5c215ec7838b647f30e798c3232c14ee9
poornima maddipat <poornimamaddipati25@gmail.com>
2025-05-28 22:39:41 +0530

    some changes of importing

--- src/components/DownloadReportPage.jsx
+++ src/components/DownloadReportPage.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-const baseURL = "https://web-production-f90ec.up.railway.app/"; // Replace with actual backend URL
-
 import "../styles/download.css";
-
+const baseURL = "https://web-production-f90ec.up.railway.app/"; // Replace with actual backend URL
 const DownloadReportPage = () => {
     const { analysisId } = useParams();
 
